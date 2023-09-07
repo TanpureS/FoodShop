a8b789605ce916975d8f806884e36a727f2337b5
Shivaji Tanpure <tanpureshivaji@gmail.com>
2023-09-07 14:17:12 +0100

    initial commit

---
+++ FoodApp/FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Shivaji Tanpure on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
